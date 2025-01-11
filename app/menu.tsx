91add62137b8982c3b8aa17192bc97ee28d78de6
cole <chloe.peiying.lin@gmail.com>
2025-01-10 16:37:43 -0800

    better formatting

--- app/menu.tsx
+++ app/menu.tsx
@@ -1,26 +1,29 @@
 export default function Menu() {
     return (
-	<nav className="bg-primary dark:bg-primary-dark border-b border-foreground/50 w-full flex justify-between">
-	    <span className="hidden md:flex">
-		<MenuItem>lists</MenuItem>
-	    	<MenuItem>create</MenuItem>
-	    	<MenuItem>about</MenuItem>
-	    	<MenuItem>feedback</MenuItem>
-	    </span>
-	    <span className="flex md:hidden">
-		<MenuItem>lists</MenuItem>
-	    	<MenuItem>you smal</MenuItem>
-	    	<MenuItem>about</MenuItem>
-	    	<MenuItem>feedback</MenuItem>
-	    </span>
-	    <MenuItem>login?</MenuItem>
-	</nav>
+        <>
+            <div className="text-center text-3xl mt-5"><a href="#">Lexasaurus</a></div>
+    	    <nav className="bg-background border-b border-midground w-full flex justify-between -mt-3">
+	        <span className="hidden md:flex">
+	    	    <MenuItem link="#">lists</MenuItem>
+	            <MenuItem link="#">create</MenuItem>
+	            <MenuItem link="#">about</MenuItem>
+	            <MenuItem link="#">feedback</MenuItem>
+	        </span>
+	        <span className="flex md:hidden">
+	    	    <MenuItem link="#">lists</MenuItem>
+	            <MenuItem link="#">you smal</MenuItem>
+	            <MenuItem link="#">about</MenuItem>
+	            <MenuItem link="#">feedback</MenuItem>
+	        </span>
+	        <MenuItem link="#">login?</MenuItem>
+	    </nav>
+        </>
     );
 }
 
-function MenuItem({children}) {
+function MenuItem({children, link}: {children: React.ReactNode, link: string}) {
     return (
-	<a className="bg-primary dark:bg-primary-dark px-5 py-3 hover:brightness-125 list-none">
+	<a href={link} className="bg-background px-5 py-3 hover:brightness-125 list-none">
 	    {children}
 	</a>
     )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ export default {
       colors: {
         "background": "var(--background)",
         "foreground": "var(--foreground)",
+        "midground": "var(--midground)",
 	"primary": "var(--primary)",
 	"secondary": "var(--secondary)",
       },
