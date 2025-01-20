29abf631edbe30e74eb1f943995f915ef11ef35d
undefynedd <chloe.peiying.lin@gmail.com>
2025-01-20 09:27:15 -0800

    fix hover triggering on mobile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 export default {
   content: [
@@ -22,5 +23,46 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addVariant, addUtilities, matchVariant }) => {
+        // Hover media queries
+        addVariant("has-hover", "@media (hover: hover) and (pointer: fine)")
+        addVariant("no-hover", "@media not all and (hover: hover) and (pointer: fine)")
+
+        // Applied on hover if supported, never applied otherwise
+        addVariant("hover-never", "@media (hover: hover) and (pointer: fine) { &:hover }")
+        matchVariant(
+            "group-hover-never",
+            (_, { modifier }) => `@media (hover: hover) and (pointer: fine) { :merge(.group${modifier ? "\\/" + modifier : ""}):hover & }`,
+            { values: { DEFAULT: "" } },
+        )
+        matchVariant(
+            "peer-hover-never",
+            (_, { modifier }) => `@media (hover: hover) and (pointer: fine) { :merge(.peer${modifier ? "\\/" + modifier : ""}):hover & }`,
+            { values: { DEFAULT: "" } },
+        )
+
+        // Applied on hover if supported, always applied otherwise
+        addVariant("hover-always", [
+            "@media (hover: hover) and (pointer: fine) { &:hover }",
+            "@media not all and (hover: hover) and (pointer: fine)",
+        ])
+        matchVariant(
+            "group-hover-always",
+            (_, { modifier }) => [
+                `@media (hover: hover) and (pointer: fine) { :merge(.group${modifier ? "\\/" + modifier : ""}):hover & }`,
+                "@media not all and (hover: hover) and (pointer: fine)",
+            ],
+            { values: { DEFAULT: "" } },
+        )
+        matchVariant(
+            "peer-hover-always",
+            (_, { modifier }) => [
+                `@media (hover: hover) and (pointer: fine) { :merge(.peer${modifier ? "\\/" + modifier : ""}):hover & }`,
+                "@media not all and (hover: hover) and (pointer: fine)",
+            ],
+            { values: { DEFAULT: "" } },
+        )
+    }),
+  ],
 } satisfies Config;
